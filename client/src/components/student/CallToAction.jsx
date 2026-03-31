99d424149dea1fab3fd2f06fc569a29dc2c87f7a
TheAshish07 <ashsah.cs.eng@gmail.com>
2026-03-31 23:56:49 +0545

    LMS using MERN

---
+++ client/src/components/student/CallToAction.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CallToAction = () => {
+  return (
+    <div>
+      <h1>Call to async function name(params) {
+        
+      }</h1>
+    </div>
+  )
+}
+
+export default CallToAction
