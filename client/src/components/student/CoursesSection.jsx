28e6b2263c18b9e44c5034f866e1635e3e1c3f05
TheAshish07 <ashsah.cs.eng@gmail.com>
2026-04-01 16:59:48 +0545

    Completed home page and CoursesList page completely

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -16,9 +16,6 @@ const {allCourses} = useContext(AppContext)
         {allCourses.slice(0,4).map((course, index)=> <CourseCard key={index} course={course} />)}
     </div>
 
-    <Link to={'/course-list'} onClick={()=> scrollTo(0,0)}
-    className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'
-    >Show all courses</Link>
     </div>
   )
 }
