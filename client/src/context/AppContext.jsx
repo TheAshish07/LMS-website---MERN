6fd0130f317aba1ac71690b723866246f29c54c8
TheAshish07 <ashsah.cs.eng@gmail.com>
2026-04-01 17:00:45 +0545

    done with home page and CoursesList page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,17 +1,32 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
     
     const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate()
 
     const [allCourses, setAllCourses] = useState([])
+    const [isEductor, setIsEductor] = useState(true)
 
     // Fetch all courses
     const fetchAllCourses = async () => {
         setAllCourses(dummyCourses)
+    }
+
+    // function to calculate average ratinf of course
+    const calculateRating = (course) => {
+        if(course.courseRatings.length === 0){
+            return 0;
+        }
+        let totalRating = 0;
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating
+        })
+        return (totalRating / course.courseRatings.length).toFixed(1);
     }
 
 useEffect(()=> {
@@ -19,7 +34,8 @@ useEffect(()=> {
 },[])
 
      const value = {
-        currency, allCourses
+        currency, allCourses, navigate, calculateRating,
+        isEductor, setIsEductor
     }
 
     return (
