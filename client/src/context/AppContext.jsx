2f8bd53282ad471cc098e5bef24cf265429338de
TheAshish07 <ashsah.cs.eng@gmail.com>
2026-04-02 17:53:30 +0545

    almost 90% CourseDetails done

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,8 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from 'humanize-duration'
+import CourseDetails from "../pages/student/CourseDetails";
 
 export const AppContext = createContext()
 
@@ -29,13 +31,42 @@ export const AppContextProvider = (props) => {
         return (totalRating / course.courseRatings.length).toFixed(1);
     }
 
+    // function to calculate coursr chapter time
+    const calculateChapterTime = (chapter) => {
+        let time = 0
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+        return humanizeDuration(time * 60 * 1000, {units: ["h", "m"]})
+    }
+
+    // function to calculate total course duration
+    const calculateCourseDuration = (course) => {
+        let time = 0
+        
+        course.courseContent.map((chapter)=> chapter.chapterContent.map(
+            (lecture) => time += lecture.lectureDuration 
+        ))
+        return humanizeDuration(time * 60 * 1000, {units: ["h", "m"]})
+    }
+
+    // function to calculate no of lectures in the course
+    const calculateNoOfLectures = (course) => {
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)){
+                totalLectures += chapter.chapterContent.length
+            }
+        })
+        return totalLectures;
+    }
+
+
 useEffect(()=> {
     fetchAllCourses()
 },[])
 
      const value = {
         currency, allCourses, navigate, calculateRating,
-        isEductor, setIsEductor
+        isEductor, setIsEductor, calculateNoOfLectures, calculateCourseDuration, calculateChapterTime
     }
 
     return (
